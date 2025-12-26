8caf0062a2d7444eaa367ba3309ba5cfc3b2b604
René de Leeuw <renedeleeuw66@gmail.com>
2025-12-26 14:07:02 +0100

    query parameters

--- FastAPI/tutorial_user_guide/main.py
+++ FastAPI/tutorial_user_guide/main.py
@@ -2,12 +2,9 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
-@app.get("/users/me")
-async def read_user_me():
-    return {"user_id": "the current user"}
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
 
-@app.get("/users/{user_id}")
-async def read_user(user_id: str):
-    return {"user_id": user_id}
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
