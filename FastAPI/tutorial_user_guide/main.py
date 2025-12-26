f5613a01e51e3a9aa5918100a34278e40422ba77
René de Leeuw <renedeleeuw66@gmail.com>
2025-12-26 15:03:55 +0100

    request body with processing

--- FastAPI/tutorial_user_guide/main.py
+++ FastAPI/tutorial_user_guide/main.py
@@ -14,4 +14,8 @@ app = FastAPI()
 
 @app.post("/items/")
 async def create_item(item: Item):
-    return item
+    item_dict = item.model_dump()
+    if item.tax is not None:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
