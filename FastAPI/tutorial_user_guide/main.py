d3871a76bd4aba9b6b4cb62bd71ea9d57e845771
René de Leeuw <renedeleeuw66@gmail.com>
2025-12-26 15:07:47 +0100

    request body and path parameters

--- FastAPI/tutorial_user_guide/main.py
+++ FastAPI/tutorial_user_guide/main.py
@@ -12,10 +12,6 @@ class Item(BaseModel):
 app = FastAPI()
 
 
-@app.post("/items/")
-async def create_item(item: Item):
-    item_dict = item.model_dump()
-    if item.tax is not None:
-        price_with_tax = item.price + item.tax
-        item_dict.update({"price_with_tax": price_with_tax})
-    return item_dict
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    return {"item_id": item_id, **item.model_dump()}
