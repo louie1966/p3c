3fa72cfef71f7cf32a40133749462de399e60523
René de Leeuw <renedeleeuw66@gmail.com>
2025-12-30 21:12:10 +0100

    query parameters and string validations with alias

--- FastAPI/tutorial_user_guide/main.py
+++ FastAPI/tutorial_user_guide/main.py
@@ -6,16 +6,7 @@ app = FastAPI()
 
 
 @app.get("/items/")
-async def read_items(
-    q: Annotated[
-        str | None,
-        Query(
-            title="Query string",
-            description="Query string for the items to search in the database that have a good match",
-            min_length=3,
-        ),
-    ] = None,
-):
+async def read_items(q: Annotated[str | None, Query(alias="item-query")] = None):
     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
     if q:
         results.update({"q": q})
