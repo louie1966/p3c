6bd50faedba30b0141d3de44c7123f9dedc5bb6e
René de Leeuw <renedeleeuw66@gmail.com>
2025-12-30 20:23:17 +0100

    query parameters and string validations add more validations

--- FastAPI/tutorial_user_guide/main.py
+++ FastAPI/tutorial_user_guide/main.py
@@ -1,10 +1,14 @@
+from typing import Annotated
+
 from fastapi import FastAPI, Query
 
 app = FastAPI()
 
 
 @app.get("/items/")
-async def read_items(q: str | None = Query(default=None, max_length=50)):
+async def read_items(
+    q: Annotated[str | None, Query(min_length=3, max_length=50)] = None,
+):
     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
     if q:
         results.update({"q": q})
