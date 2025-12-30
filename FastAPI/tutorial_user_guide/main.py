2712d09fcf353b825b53cf0dbff5b50231614e1b
René de Leeuw <renedeleeuw66@gmail.com>
2025-12-30 20:57:51 +0100

    query parameters and string validations list with defaults

--- FastAPI/tutorial_user_guide/main.py
+++ FastAPI/tutorial_user_guide/main.py
@@ -6,6 +6,6 @@ app = FastAPI()
 
 
 @app.get("/items/")
-async def read_items(q: Annotated[list[str] | None, Query()] = None):
+async def read_items(q: Annotated[list[str], Query()] = ["foo", "bar"]):
     query_items = {"q": q}
     return query_items
