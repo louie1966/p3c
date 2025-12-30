ce50ab51f872d712706f11504dde9ae3df9c82dc
René de Leeuw <renedeleeuw66@gmail.com>
2025-12-30 21:38:07 +0100

    path parameters and numeric validations

--- FastAPI/tutorial_user_guide/main.py
+++ FastAPI/tutorial_user_guide/main.py
@@ -1,30 +1,16 @@
-import random
 from typing import Annotated
 
-from fastapi import FastAPI
-from pydantic import AfterValidator
+from fastapi import FastAPI, Path, Query
 
 app = FastAPI()
 
-data = {
-    "isbn-9781529046137": "The Hitchhiker's Guide to the Galaxy",
-    "imdb-tt0371724": "The Hitchhiker's Guide to the Galaxy",
-    "isbn-9781439512982": "Isaac Asimov: The Complete Stories, Vol. 2",
-}
 
-
-def check_valid_id(id: str):
-    if not id.startswith(("isbn-", "imdb-")):
-        raise ValueError('Invalid ID format, it must start with "isbn-" or "imdb-"')
-    return id
-
-
-@app.get("/items/")
+@app.get("/items/{item_id}")
 async def read_items(
-    id: Annotated[str | None, AfterValidator(check_valid_id)] = None,
+    item_id: Annotated[int, Path(title="The ID of the item to get")],
+    q: Annotated[str | None, Query(alias="item-query")] = None,
 ):
-    if id:
-        item = data.get(id)
-    else:
-        id, item = random.choice(list(data.items()))
-    return {"id": id, "name": item}
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    return results
