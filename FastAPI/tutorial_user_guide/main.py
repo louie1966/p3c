3e172f6b3f17928f58f220a56410a4df165d5a32
René de Leeuw <renedeleeuw66@gmail.com>
2025-12-31 12:56:25 +0100

    query parameter models with a pydantic model forbid extra query parameters

--- FastAPI/tutorial_user_guide/main.py
+++ FastAPI/tutorial_user_guide/main.py
@@ -7,6 +7,8 @@ app = FastAPI()
 
 
 class FilterParams(BaseModel):
+    model_config = {"extra": "forbid"}
+
     limit: int = Field(100, gt=0, le=100)
     offset: int = Field(0, ge=0)
     order_by: Literal["created_at", "updated_at"] = "created_at"
