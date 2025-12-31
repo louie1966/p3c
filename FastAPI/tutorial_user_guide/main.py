40014d057c5d62a380ae11b11b587f9b8b476c57
René de Leeuw <renedeleeuw66@gmail.com>
2025-12-31 13:06:05 +0100

    body - multiple parameters mix path query and body parameters

--- FastAPI/tutorial_user_guide/main.py
+++ FastAPI/tutorial_user_guide/main.py
@@ -1,20 +1,27 @@
-from typing import Annotated, Literal
+from typing import Annotated
 
-from fastapi import FastAPI, Query
-from pydantic import BaseModel, Field
+from fastapi import FastAPI, Path
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-class FilterParams(BaseModel):
-    model_config = {"extra": "forbid"}
-
-    limit: int = Field(100, gt=0, le=100)
-    offset: int = Field(0, ge=0)
-    order_by: Literal["created_at", "updated_at"] = "created_at"
-    tags: list[str] = []
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
 
 
-@app.get("/items/")
-async def read_items(filter_query: Annotated[FilterParams, Query()]):
-    return filter_query
+@app.put("/items/{item_id}")
+async def update_item(
+    item_id: Annotated[int, Path(title="The ID of the item to get", ge=0, le=1000)],
+    q: str | None = None,
+    item: Item | None = None,
+):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    if item:
+        results.update({"item": item})
+    return results
