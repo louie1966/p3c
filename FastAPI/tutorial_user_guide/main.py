f531d97ec6092258aba0c384a9fffd7e51a94aa0
René de Leeuw <renedeleeuw66@gmail.com>
2025-12-31 12:26:50 +0100

    path parameters and numeric validations order is not important other variant 2

--- FastAPI/tutorial_user_guide/main.py
+++ FastAPI/tutorial_user_guide/main.py
@@ -1,14 +1,10 @@
-from typing import Annotated
-
 from fastapi import FastAPI, Path
 
 app = FastAPI()
 
 
 @app.get("/items/{item_id}")
-async def read_items(
-    q: str, item_id: Annotated[int, Path(title="The ID of the item to get")]
-):
+async def read_items(*, item_id: int = Path(title="The ID of the item to get"), q: str):
     results = {"item_id": item_id}
     if q:
         results.update({"q": q})
