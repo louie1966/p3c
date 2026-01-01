82880de2d1cc31154cd292b822e18ed348d28059
René de Leeuw <renedeleeuw66@gmail.com>
2026-01-01 18:38:30 +0100

    body - fields declare model attributes

--- FastAPI/tutorial_user_guide/main.py
+++ FastAPI/tutorial_user_guide/main.py
@@ -1,15 +1,17 @@
 from typing import Annotated
 
 from fastapi import Body, FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
 
 class Item(BaseModel):
     name: str
-    description: str | None = None
-    price: float
+    description: str | None = Field(
+        default=None, title="The description of the item", max_length=300
+    )
+    price: float = Field(gt=0, description="The price must be greater than zero")
     tax: float | None = None
 
 
