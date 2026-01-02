0f29fd9576c7097e20450566616a3ed21ed6d2fb
René de Leeuw <renedeleeuw66@gmail.com>
2026-01-02 16:01:19 +0100

    form data define form parameters

--- FastAPI/tutorial_user_guide/main.py
+++ FastAPI/tutorial_user_guide/main.py
@@ -1,8 +1,10 @@
-from fastapi import FastAPI, status
+from typing import Annotated
+
+from fastapi import FastAPI, Form
 
 app = FastAPI()
 
 
-@app.post("/items/", status_code=status.HTTP_201_CREATED)
-async def create_item(name: str):
-    return {"name": name}
+@app.post("/login/")
+async def login(username: Annotated[str, Form()], password: Annotated[str, Form()]):
+    return {"username": username}
