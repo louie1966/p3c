986f61613567e823662eee48d298557e490b8091
René de Leeuw <renedeleeuw66@gmail.com>
2026-01-02 05:11:40 +0100

    header parameters strange header conversion

--- FastAPI/tutorial_user_guide/main.py
+++ FastAPI/tutorial_user_guide/main.py
@@ -6,5 +6,7 @@ app = FastAPI()
 
 
 @app.get("/items/")
-async def read_items(user_agent: Annotated[str | None, Header()] = None):
-    return {"User-Agent": user_agent}
+async def read_items(
+    strange_header: Annotated[str | None, Header(convert_underscores=False)] = None,
+):
+    return {"strange_header": strange_header}
