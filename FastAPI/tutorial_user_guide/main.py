6d4cf035ea21b3e02d748090bc1dfbe35fc3006a
René de Leeuw <renedeleeuw66@gmail.com>
2026-01-02 04:40:17 +0100

    declare request example data field additional arguments

--- FastAPI/tutorial_user_guide/main.py
+++ FastAPI/tutorial_user_guide/main.py
@@ -1,27 +1,14 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
 
 class Item(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    tax: float | None = None
-
-    model_config = {
-        "json_schema_extra": {
-            "examples": [
-                {
-                    "name": "Foo",
-                    "description": "A very nice Item",
-                    "price": 35.4,
-                    "tax": 3.2,
-                }
-            ]
-        }
-    }
+    name: str = Field(examples=["Foo"])
+    description: str | None = Field(default=None, examples=["A very nice Item"])
+    price: float = Field(examples=[35.4])
+    tax: float | None = Field(default=None, examples=[3.2])
 
 
 @app.put("/items/{item_id}")
