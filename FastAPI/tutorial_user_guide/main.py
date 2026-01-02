2b29a3e6931075927e197e95bdd4aa04cf8a289b
René de Leeuw <renedeleeuw66@gmail.com>
2026-01-02 04:43:18 +0100

    declare request example data body with examples

--- FastAPI/tutorial_user_guide/main.py
+++ FastAPI/tutorial_user_guide/main.py
@@ -1,17 +1,34 @@
-from fastapi import FastAPI
-from pydantic import BaseModel, Field
+from typing import Annotated
+
+from fastapi import Body, FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
 class Item(BaseModel):
-    name: str = Field(examples=["Foo"])
-    description: str | None = Field(default=None, examples=["A very nice Item"])
-    price: float = Field(examples=[35.4])
-    tax: float | None = Field(default=None, examples=[3.2])
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
 
 
 @app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Item):
+async def update_item(
+    item_id: int,
+    item: Annotated[
+        Item,
+        Body(
+            examples=[
+                {
+                    "name": "Foo",
+                    "description": "A very nice Item",
+                    "price": 35.4,
+                    "tax": 3.2,
+                }
+            ],
+        ),
+    ],
+):
     results = {"item_id": item_id, "item": item}
     return results
