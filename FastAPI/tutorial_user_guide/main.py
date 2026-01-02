f5e9c45ab80cd30db852be206bc806a55e04d0d6
René de Leeuw <renedeleeuw66@gmail.com>
2026-01-02 04:49:38 +0100

    declare request example data body with openapi-specific examples

--- FastAPI/tutorial_user_guide/main.py
+++ FastAPI/tutorial_user_guide/main.py
@@ -20,22 +20,33 @@ async def update_item(
     item: Annotated[
         Item,
         Body(
-            examples=[
-                {
-                    "name": "Foo",
-                    "description": "A very nice Item",
-                    "price": 35.4,
-                    "tax": 3.2,
+            openapi_examples={
+                "normal": {
+                    "summary": "A normal example",
+                    "description": "A **normal** item works correctly.",
+                    "value": {
+                        "name": "Foo",
+                        "description": "A very nice Item",
+                        "price": 35.4,
+                        "tax": 3.2,
+                    },
                 },
-                {
-                    "name": "Bar",
-                    "price": "35.4",
+                "converted": {
+                    "summary": "An example with converted data",
+                    "description": "FastAPI can convert price `strings` to actual `numbers` automatically",
+                    "value": {
+                        "name": "Bar",
+                        "price": "35.4",
+                    },
                 },
-                {
-                    "name": "Baz",
-                    "price": "thirty five point four",
+                "invalid": {
+                    "summary": "Invalid data is rejected with an error",
+                    "value": {
+                        "name": "Baz",
+                        "price": "thirty five point four",
+                    },
                 },
-            ],
+            },
         ),
     ],
 ):
