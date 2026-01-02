30e76c7d8500ee7667bd11f30c9a9e8adc1c187c
René de Leeuw <renedeleeuw66@gmail.com>
2026-01-02 04:18:08 +0100

    body - nested models bodies of pure lists

--- FastAPI/tutorial_user_guide/main.py
+++ FastAPI/tutorial_user_guide/main.py
@@ -9,22 +9,6 @@ class Image(BaseModel):
     name: str
 
 
-class Item(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    tax: float | None = None
-    tags: set[str] = set()
-    images: list[Image] | None = None
-
-
-class Offer(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    items: list[Item]
-
-
-@app.post("/offers/")
-async def create_offer(offer: Offer):
-    return offer
+@app.post("/images/multiple/")
+async def create_multiple_images(images: list[Image]):
+    return images
