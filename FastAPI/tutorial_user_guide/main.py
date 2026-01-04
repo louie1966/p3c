e3c49af5e5fc3ec2f950944b37dad1bfb3780353
René de Leeuw <renedeleeuw66@gmail.com>
2026-01-04 15:09:32 +0100

    dependencies share annotated dependencies

--- FastAPI/tutorial_user_guide/main.py
+++ FastAPI/tutorial_user_guide/main.py
@@ -9,11 +9,14 @@ async def common_parameters(q: str | None = None, skip: int = 0, limit: int = 10
     return {"q": q, "skip": skip, "limit": limit}
 
 
+CommonsDep = Annotated[dict, Depends(common_parameters)]
+
+
 @app.get("/items/")
-async def read_items(commons: Annotated[dict, Depends(common_parameters)]):
+async def read_items(commons: CommonsDep):
     return commons
 
 
 @app.get("/users/")
-async def read_users(commons: Annotated[dict, Depends(common_parameters)]):
+async def read_users(commons: CommonsDep):
     return commons
