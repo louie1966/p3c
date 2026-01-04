c9724b164cfa586c87aa7e946d99c9df4478ee6e
René de Leeuw <renedeleeuw66@gmail.com>
2026-01-04 16:08:45 +0100

    dependencies global

--- FastAPI/tutorial_user_guide/main.py
+++ FastAPI/tutorial_user_guide/main.py
@@ -2,8 +2,6 @@ from typing import Annotated
 
 from fastapi import Depends, FastAPI, Header, HTTPException
 
-app = FastAPI()
-
 
 async def verify_token(x_token: Annotated[str, Header()]):
     if x_token != "fake-super-secret-token":
@@ -16,6 +14,14 @@ async def verify_key(x_key: Annotated[str, Header()]):
     return x_key
 
 
-@app.get("/items/", dependencies=[Depends(verify_token), Depends(verify_key)])
+app = FastAPI(dependencies=[Depends(verify_token), Depends(verify_key)])
+
+
+@app.get("/items/")
 async def read_items():
-    return [{"item": "Foo"}, {"item": "Bar"}]
+    return [{"item": "Portal Gun"}, {"item": "Plumbus"}]
+
+
+@app.get("/users/")
+async def read_users():
+    return [{"username": "Rick"}, {"username": "Morty"}]
