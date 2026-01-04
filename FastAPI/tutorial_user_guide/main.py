dcf612c4125dd41ba704b10d274ccabd0cefc252
René de Leeuw <renedeleeuw66@gmail.com>
2026-01-04 15:19:20 +0100

    classes as dependencies a dict of previous example

--- FastAPI/tutorial_user_guide/main.py
+++ FastAPI/tutorial_user_guide/main.py
@@ -9,14 +9,11 @@ async def common_parameters(q: str | None = None, skip: int = 0, limit: int = 10
     return {"q": q, "skip": skip, "limit": limit}
 
 
-CommonsDep = Annotated[dict, Depends(common_parameters)]
-
-
 @app.get("/items/")
-async def read_items(commons: CommonsDep):
+async def read_items(commons: Annotated[dict, Depends(common_parameters)]):
     return commons
 
 
 @app.get("/users/")
-async def read_users(commons: CommonsDep):
+async def read_users(commons: Annotated[dict, Depends(common_parameters)]):
     return commons
