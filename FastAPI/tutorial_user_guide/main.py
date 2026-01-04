6a842bcff8809ae73e1e01624b4147facfc5dc8a
René de Leeuw <renedeleeuw66@gmail.com>
2026-01-04 16:26:31 +0100

    dependencies early exit and scope

--- FastAPI/tutorial_user_guide/main.py
+++ FastAPI/tutorial_user_guide/main.py
@@ -1,30 +1,17 @@
 from typing import Annotated
 
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import Depends, FastAPI
 
 app = FastAPI()
-
-
-class InternalError(Exception):
-    pass
 
 
 def get_username():
     try:
         yield "Rick"
-    except InternalError:
-        print("We don't swallow the internal error here, we raise again 😎")
-        raise
+    finally:
+        print("Cleanup up before response is sent")
 
 
-@app.get("/items/{item_id}")
-def get_item(item_id: str, username: Annotated[str, Depends(get_username)]):
-    if item_id == "portal-gun":
-        raise InternalError(
-            f"The portal gun is too dangerous to be owned by {username}"
-        )
-    if item_id != "plumbus":
-        raise HTTPException(
-            status_code=404, detail="Item not found, there's only a plumbus here"
-        )
-    return item_id
+@app.get("/users/me")
+def get_user_me(username: Annotated[str, Depends(get_username, scope="function")]):
+    return username
