732776e6c2a3c08182c7b98e0141b5d5485f1590
René de Leeuw <renedeleeuw66@gmail.com>
2026-01-23 22:14:28 +0100

    build the filetree with examples

---
+++ FastAPI/Tutorial - User Guide/02-Path Parameters 3/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/users")
+async def read_default_user():
+    return {"user_id": "the system user"}
+
+
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
